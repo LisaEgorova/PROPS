f5f2688778aecff2ebeca8cdda155d9012cacf71
Elisaveta Egorova <eliza2804@bk.ru>
2025-02-08 10:29:11 +0300

    Initial Commit

---
+++ PROPS/PROPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PROPSApp.swift
+//  PROPS
+//
+//  Created by Elisaveta Egorova on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PROPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PROPSTests/PROPSTests.swift
@@ -0,0 +1,17 @@
+//
+//  PROPSTests.swift
+//  PROPSTests
+//
+//  Created by Elisaveta Egorova on 08.02.2025.
+//
+
+import Testing
+@testable import PROPS
+
+struct PROPSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
